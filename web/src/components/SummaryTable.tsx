b1ad26d3384b785742038f5d3b06eab9710f5a57
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-18 18:00:29 -0300

    feat: minimumSummaryDatesSize create

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -13,6 +13,9 @@ const weekDays = [
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export function SummaryTable(){
   return (
     <div className="w-full flex">
@@ -32,6 +35,15 @@ export function SummaryTable(){
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
           return <HabitDay key={date.toString()}/>
+        })}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
+          return (
+            <div key={i}
+            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+            />
+            
+          )
         })}
       </div>
 
