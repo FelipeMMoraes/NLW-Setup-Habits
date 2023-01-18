8b3a2ed2f1724e18dd44af671d85230502033716
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-18 17:39:54 -0300

    Feat: create component habitDays and SummaryTable

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/global.css'
 import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 
 // import { Habit } from "./components/Habit";
@@ -11,7 +12,8 @@ export function App() {
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
 
         <Header />
-
+        <SummaryTable />
+        
       </div>
     </div>
   )
