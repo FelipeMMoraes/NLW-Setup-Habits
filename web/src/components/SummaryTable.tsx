9dacf9bfa176d7f9199ae91561273b407e86460e
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-19 17:38:53 -0300

    feat: component Header, ProgressBar

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -42,7 +42,6 @@ export function SummaryTable(){
             <div key={i}
             className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
             />
-            
           )
         })}
       </div>
