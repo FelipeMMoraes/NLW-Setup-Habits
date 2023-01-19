533a8444af012b38de93fadaaf1813e5e31b1ccb
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-19 14:28:37 -0300

    feat: muita coisa nao lembro tudo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View, StatusBar  } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter'
 
 import { Loading } from './src/components/Loading';
+
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,22 +20,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Teste Teste Teste Teste</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
