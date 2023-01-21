eeedd11a52081f41449cc737b102048f6f47238a
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-21 14:26:55 -0300

    feat: componenent backcbutton and input New

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,25 @@
-import { View } from "react-native";
+import { ScrollView, View, Text, TextInput } from "react-native";
+import { Backbutton } from "../components/Backbutton";
+
 
 export function New() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      
+      <ScrollView showsVerticalScrollIndicator={false}>
+        
+        <Backbutton />
+
+        <Text className='mt-6 text-white font-extrabold text-3xl'>
+          Criar hábito
+        </Text>
+
+        <Text className='mt-6 text-white font-semibold text-base'>
+          Qual seu comprometimento?
+        </Text>
+
+        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"/>
+
+      </ScrollView>
     </View>
   )
 }
