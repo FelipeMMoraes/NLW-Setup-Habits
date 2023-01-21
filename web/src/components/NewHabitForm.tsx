da7069862ab1d4f41d61a9de435d892736197931
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-21 20:36:54 -0300

    feat: connect apiwith frontend part 1

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
 
 const avaiableWeekDays = [
   'Domingo',
@@ -17,9 +18,22 @@ export function NewHabitForm() {
   const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
-  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault()
 
+    if(!title || weekDays.length === 0){
+      return
+    }
+
+    await api.post('habits', {
+      title,
+      weekDays
+    })
+
+    setTitle('');
+    setWeekDays([]);
+
+    alert('Hábito criado com sucesso!')
   }
 
   function handleToggleWeekDay(weekDay: number){
@@ -47,6 +61,7 @@ export function NewHabitForm() {
       placeholder="ex.: Exercícios, dormir bem, etc..."
       className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
       autoFocus
+      value={title}
       onChange={event => setTitle(event.target.value)}
       />
 
@@ -61,6 +76,7 @@ export function NewHabitForm() {
               <Checkbox.Root
                 key={weekDay}
                 className='flex items-center gap-3 group'
+                checked={weekDays.includes(index)}
                 onCheckedChange={() => handleToggleWeekDay(index)}
               >
               
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay"
 
@@ -16,15 +19,32 @@ const summaryDates = generateDatesFromYearBeginning()
 const minimumSummaryDatesSize = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
-export function SummaryTable(){
+type Summary ={
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[]
+
+export function SummaryTable() {
+
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    api.get('/summary').then(response => {
+      setSummary(response.data)
+    })
+  }, [])
+
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay, i) =>{
-          return(
+        {weekDays.map((weekDay, i) => {
+          return (
             <div
               key={`${weekDay}-${i}`}
-              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center" 
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
             >
               {weekDay}
             </div>
@@ -34,19 +54,24 @@ export function SummaryTable(){
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
+
           return (
-          <HabitDay
-            key={date.toString()}
-            amount={5}
-            completed={Math.round(Math.random() * 5)}
+            <HabitDay
+              key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           )
         })}
 
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
           return (
             <div key={i}
-            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
             />
           )
         })}
